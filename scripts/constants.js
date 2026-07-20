82598c7e2a2dcdae99fd536625001d7d285a859b
terrenusest-lang <terrenus.est@gmail.com>
2026-07-20 23:16:15 +0300

    Add shared module constants

---
+++ scripts/constants.js
@@ -0,0 +1,5 @@
+export const MODULE_ID = "material-component-checker";
+export const COMPONENT_PACK = "world.material-components";
+export const INDEX_SETTING = "spellComponentIndex";
+export const REVIEW_SETTING = "manualReview";
+export const POUCH_KEY = "spell-component-pouch";
